7e67821abea041cc9bfa359733c172840efca0bf
Kim Syversen <kim.syversen@gmail.com>
2015-03-06 12:14:20 +0100

    Improved implementation of footer

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -1,13 +1,13 @@
-<div class="footer">
-    <div class="col-md-12 footer-item">
-        <h4>Some placeholder text</h4>
-        <ul class="footer-list">
-            <li class="footer-list-item">Placeholder text</li>
-            <li class="footer-list-item">Placeholder text</li>
-            <li class="footer-list-item">Placeholder text</li>
-            <li class="footer-list-item">Placeholder text</li>
-            <li class="footer-list-item">Placeholder text</li>
-        </ul>
+<div class="navbar navbar-default navbar-static-bottom footer-nav">
+    <div class="col-sm-12 col-md-12 footer-item">
+    <h4>Some placeholder text</h4>
+    <ul class="list-unstyled">
+        <li class="footer-list-item">Placeholder text</li>
+        <li class="footer-list-item">Placeholder text</li>
+        <li class="footer-list-item">Placeholder text</li>
+        <li class="footer-list-item">Placeholder text</li>
+        <li class="footer-list-item">Placeholder text</li>
+    </ul>
     </div>
-
 </div>
+
