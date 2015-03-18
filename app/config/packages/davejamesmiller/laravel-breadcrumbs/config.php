c4505ee64d88c24fab9344565b4b68e068456f82
Kim Syversen <kim.syversen@gmail.com>
2015-03-18 13:41:47 +0100

    Breadcrumb custom template

--- app/config/packages/davejamesmiller/laravel-breadcrumbs/config.php
+++ app/config/packages/davejamesmiller/laravel-breadcrumbs/config.php
@@ -1,5 +1,5 @@
 <?php
 
 return array(
-    'view' => 'laravel-breadcrumbs::bootstrap3',
+    'view' => 'conference._partials.breadcrumb',
 );
---
+++ app/views/conference/_partials/breadcrumb.blade.php
@@ -0,0 +1,12 @@
+
+@if ($breadcrumbs)
+    <ul class="breadcrumb conference-breacrumb">
+        @foreach ($breadcrumbs as $breadcrumb)
+            @if (!$breadcrumb->last)
+                <li><a href="{{{ $breadcrumb->url }}}">{{{ $breadcrumb->title }}}</a></li>
+            @else
+                <li class="active">{{{ $breadcrumb->title }}}</li>
+            @endif
+        @endforeach
+    </ul>
+@endif
