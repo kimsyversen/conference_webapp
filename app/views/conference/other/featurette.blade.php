d13ed0489a3eb9d3d069037f654e6b35ab528619
Kim Syversen <kim.syversen@gmail.com>
2015-03-19 09:55:51 +0100

    Adjustments to featurette

--- app/views/conference/other/featurette.blade.php
+++ app/views/conference/other/featurette.blade.php
@@ -1,25 +1,5 @@
 @extends('conference.layouts.default')
 @section('content')
-{{--
-    <div class="featurette">
-        <figure class="row">
-            <span class="glyphicon glyphicon-pencil pull-left" aria-hidden="true"></span>
-            <p class="lead text-center">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </figure>
-    </div>
-
-
-    <div class="featurette">
-        <figure class="row">
-            <span class="glyphicon glyphicon-pencil pull-left" aria-hidden="true"></span>
-            <p class="lead text-center">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </figure>
-    </div>
-
-
-    @include('conference.partials.goto-top')--}}
-
-
 
 <div class="featurette">
     <span class="glyphicon glyphicon-list-alt pull-left" aria-hidden="true"></span>
@@ -31,7 +11,8 @@
 
 <div class="featurette">
     <span class="glyphicon glyphicon-calendar pull-right" aria-hidden="true"></span>
-    {{--  <img class="featurette-image img-circle pull-right" src="http://placehold.it/512">--}}
+
+
     <h2 class="featurette-heading">Browse the conference program</h2>
     <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
 </div>
@@ -63,8 +44,11 @@
     <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
 </div>
 
+
 <hr class="featurette-divider">
-<!-- /END THE FEATURETTES -->
+
+
+
 @stop
 
 
