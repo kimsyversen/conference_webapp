34aaf7b97b4c7a9074ea9c6c36b66e52e845817f
Kim Syversen <kim.syversen@gmail.com>
2015-03-22 10:14:38 +0100

    Add text to features

--- app/views/conference/other/featurette.blade.php
+++ app/views/conference/other/featurette.blade.php
@@ -4,7 +4,8 @@
 <div class="featurette">
     <span class="glyphicon glyphicon-list-alt pull-left" aria-hidden="true"></span>
     <h2 class="featurette-heading">Browse conferences</h2>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
+    <p class="lead">This application can host multiple conferences. When you first visit the application, you can see all available conferences together with their location and the date. If you want to create your own conference, you can do this too. It only requires that you register an account, then you can start creating your conference program.</p>
+
 </div>
 
 <hr class="featurette-divider">
@@ -13,16 +14,19 @@
     <span class="glyphicon glyphicon-calendar pull-right" aria-hidden="true"></span>
 
 
-    <h2 class="featurette-heading">Browse the conference program</h2>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
+    <h2 class="featurette-heading">Browse the conference schedule</h2>
+    <p class="lead">Instead of carrying a brochure in your pocket with the conference program, you can now access it from your smartphone, tablet, laptop or desktop computer. If a session is cancelled or changed for some reason, you will see that too. </p>
+    <br>
+
 </div>
 
 <hr class="featurette-divider">
 
 <div class="featurette">
     <span class="glyphicon glyphicon-save pull-left" aria-hidden="true"></span>
-    <h2 class="featurette-heading">Add  sessions to your personal agenda</h2>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
+    <h2 class="featurette-heading">Add sessions to your personal agenda</h2>
+    <p class="lead">There is no need to create a list of the sessions you want to attend to. You have the possibility to add interesting sessions to your own personal agenda with just one click. However, this requires that you have created an account.</p>
+    <br>
 </div>
 
 <hr class="featurette-divider">
@@ -31,7 +35,11 @@
 <div class="featurette">
     <span class="glyphicon glyphicon-map-marker pull-right" aria-hidden="true"></span>
     <h2 class="featurette-heading">Easy access to the conference maps</h2>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
+    <p class="lead">You have access to the maps for the conference. We hope you don't get lost.</p>
+    <br>
+    <br>
+    <br><br><br>
+
 </div>
 
 <hr class="featurette-divider">
@@ -41,11 +49,23 @@
 <div class="featurette">
     <span class="glyphicon glyphicon-earphone pull-left" aria-hidden="true"></span>
     <h2 class="featurette-heading">Receive the latest news</h2>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui fringilla, congue turpis non, tincidunt ligula. Aliquam viverra nisl sed orci fringilla, nec suscipit justo molestie. Praesent imperdiet arcu neque, vitae imperdiet urna vulputate pretium. Maecenas in consectetur lorem.</p>
+    <p class="lead">The conference has a newsfeeds where you will see the latest messages from the conference administration team. The newsfeed also pulls the last tweets. Everything about the conference in one convenient place.</p>
+    <br><br>
 </div>
 
 
 <hr class="featurette-divider">
+
+<div class="featurette">
+    <span class="glyphicon glyphicon-comment pull-right" aria-hidden="true"></span>
+    <h2 class="featurette-heading">Receive private messages</h2>
+    <p class="lead">You have the possibility to receive private messages from the administrator. However, this also requires an account and that you are logged in to the application.</p>
+    <br><br>
+</div>
+
+
+<hr class="featurette-divider">
+
 
 
 
