b5c6fcb6588ba2a2922dd989456168d3bbb69413
Kim Syversen <kim.syversen@gmail.com>
2015-03-22 13:52:17 +0100

    Possible IE button fix

--- app/views/conference/layouts/partials/doFirstTimeStuff.blade.php
+++ app/views/conference/layouts/partials/doFirstTimeStuff.blade.php
@@ -6,8 +6,8 @@
                 <p class="lead">Hi! We see that this is the first time you use this application. Do you want a short presentation of what you can do with this application?</p>
                 <div class="btn-group">
 
-                    <button class="btn button-dark"><a href="{{ route('featurette_path')}}">Give me a quick tour</a> </button>
-                    <button class="btn button-dark" id="dismiss"><a href="#">Dismiss</a></button>
+                    <button type="button" class="btn button-dark"><a href="{{ route('featurette_path')}}">Give me a quick tour</a> </button>
+                    <button type="button" class="btn button-dark" id="dismiss"><a href="#">Dismiss</a></button>
                 </div>
             </div>
         </div>
