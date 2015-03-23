e75ce38155ce9970f4cb6552d697a96c258daaaf
Kim Syversen <kim.syversen@gmail.com>
2015-03-23 10:01:53 +0100

    Tweak newsfeed

--- app/views/conference/newsfeed/index.blade.php
+++ app/views/conference/newsfeed/index.blade.php
@@ -8,7 +8,8 @@
         @if(isset($data) && !empty($data))
 
             <div class="row">
-                <div class="col-md-6 col-sm-12">
+                <div class="col-sm-1 col-md-2"></div>
+                <div class="col-sm-10 col-md-8">
                     @foreach($data['data']['newsposts'] as $post)
                         @include('conference.components.message', [
                             'classes' => 'newspost-conference',
