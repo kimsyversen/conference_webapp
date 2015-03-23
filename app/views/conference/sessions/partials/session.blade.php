93acbc6ccfc776d0ed57816a426476f784f5ffc5
Kim Syversen <kim.syversen@gmail.com>
2015-03-23 16:30:35 +0100

    Remove social stuff

--- app/views/conference/sessions/partials/session.blade.php
+++ app/views/conference/sessions/partials/session.blade.php
@@ -32,18 +32,18 @@
                 </div>
             </div>
 
-            <div class="col-xs-12 col-sm-1 col-md-1 social">
+    {{--        <div class="col-xs-12 col-sm-1 col-md-1 social">
                 <div class="row text-center">
-                   {{-- <div class="fb-share-button" data-href="/{{ $session['links']['session']['uri'] }}" data-layout="icon"></div>--}}
+                   --}}{{-- <div class="fb-share-button" data-href="/{{ $session['links']['session']['uri'] }}" data-layout="icon"></div>--}}{{--
                     <div class="col-xs-4 col-sm-12 col-md-12 facebook nopadding" id="fb-root">
-                     <div class="fb-share-button" data-href="/{{ $session['links']['session']['uri'] }}" data-layout="link"></div>
+
                     </div>
 
-                    {{--<div class="col-xs-4 col-sm-12 col-md-12 facebook nopadding" id="fb-root"><a class="fb-share-button fb-root" data-href="/{{ $session['links']['session']['uri'] }}" data-layout="link"> <span class="fa fa-facebook"></span> </a>  </div>--}}
+                    --}}{{--<div class="col-xs-4 col-sm-12 col-md-12 facebook nopadding " id="fb-root"><a class="fb-share-button fb-root" data-href="/{{ $session['links']['session']['uri'] }}" data-layout="link"> <span class="fa fa-facebook"></span> </a>  </div>--}}{{--
                     <div class="col-xs-4 col-sm-12 col-md-12 twitter nopadding"><a href="#twitter"> <span class="fa fa-twitter"></span></a></div>
                     <div class="col-xs-4 col-sm-12 col-md-12 google-plus nopadding"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></div>
                 </div>
-            </div>
+            </div>--}}
         </div>
         <div class="row buttons">
             @if($schedule_type == 'conference')
