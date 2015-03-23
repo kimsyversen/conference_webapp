ec544b7c56380f096d90e72d95d7a7273d27e2b7
Kim Syversen <kim.syversen@gmail.com>
2015-03-23 13:21:49 +0100

    Tweaks

--- app/views/conference/sessions/partials/session.blade.php
+++ app/views/conference/sessions/partials/session.blade.php
@@ -1,6 +1,6 @@
 
     <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-1 col-md-10">
+        <div class="col-xs-12  col-md-offset-1 col-md-10">
             <ul class="event-list">
                 <li>
                     <time>
