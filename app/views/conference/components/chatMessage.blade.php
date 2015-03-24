3501d545ee64d36009d4e4c1bf7297e578323b20
Kim Syversen <kim.syversen@gmail.com>
2015-03-24 08:18:40 +0100

    Chat tweaks

--- app/views/conference/components/chatMessage.blade.php
+++ app/views/conference/components/chatMessage.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
     <div class="chat-message">
-        <div class="col-md-2"></div>
+        <div class="col-md-1"></div>
         <div class="col-xs-3 col-sm-2 col-md-3">
             <div class="chat-user">
                 {{( $user )}}
             </div>
         </div>
-        <div class="col-xs-9 col-sm-10 col-md-6">
+        <div class="col-xs-9 col-sm-10 col-md-7">
             <div class="row">
                 <div class="col-xs-12 text-center">
                     <span class="text-muted">{{ ConferenceHelper::formatTimestamp($time, 'D H:i:s') }}</span>
