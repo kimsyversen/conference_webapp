7e02ee7e238bfdd7a0e417a523b45a741fd1c012
Kim Syversen <kim.syversen@gmail.com>
2015-03-24 16:33:05 +0100

    Update kims hostname

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	'production' => array('turbo', 'turbo.uninett.no'),
-	'local' =>  array('Kimba', 'kImac.local' , 'administrator-VirtualBox')
+	'local' =>  array('Kimba', 'kImac' , 'administrator-VirtualBox')
 ));
 
 /*
