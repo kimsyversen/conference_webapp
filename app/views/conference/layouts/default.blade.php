413d701fd45fb7a77832552caee8bd115f23ede5
Kim Syversen <kim.syversen@gmail.com>
2015-03-29 10:31:33 +0200

    Add charset

--- app/views/conference/layouts/default.blade.php
+++ app/views/conference/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Conferencebook</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
