04690eeca323f90c72d7389745ccdc1d7c04e83c
Kim Syversen <kim.syversen@gmail.com>
2015-03-31 10:33:23 +0200

    Possible solution for sharing with facebook

--- app/views/conference/events/partials/social.blade.php
+++ app/views/conference/events/partials/social.blade.php
@@ -1,6 +1,6 @@
 
 <div class="row text-center">
-    <div class="col-xs-4 col-sm-12 col-md-12 facebook nopadding"><a href="http://www.facebook.com/sharer.php?u={{ $session['links']['session']['uri'] }}" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"> <span class="fa fa-facebook"></span> </a>  </div>
+    <div class="col-xs-4 col-sm-12 col-md-12 facebook nopadding"><a href="http://www.facebook.com/sharer.php?u=/{{ $session['links']['session']['uri'] }}" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"> <span class="fa fa-facebook"></span> </a>  </div>
 
     <div class="col-xs-4 col-sm-12 col-md-12 twitter nopadding"><a href="#twitter"> <span class="fa fa-twitter"></span></a></div>
     <div class="col-xs-4 col-sm-12 col-md-12 google-plus nopadding"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></div>
