2adfd9da866d9a2da0f16d08b3fb4cb90452137b
Kim Syversen <kim.syversen@gmail.com>
2015-03-31 16:20:58 +0200

    Add quote sign to descripton of conferences

--- app/views/conference/events/event.blade.php
+++ app/views/conference/events/event.blade.php
@@ -20,7 +20,6 @@
                     </div>
                     <div class="description-long hidden">
                         <div class="col-xs-12">
-
                             <p><i class="fa fa-quote-left text-muted" style="font-size: 2.5em; padding: 0 0.5em 0 0 ;"></i>{{ $session['description'] }}</p>
                         </div>
                     </div>
