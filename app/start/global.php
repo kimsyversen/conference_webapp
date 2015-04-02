abfe20df93f16ecca64a57b46bc9db22f64ea474
Kim Syversen <kim.syversen@gmail.com>
2015-04-02 19:35:26 +0200

    Use one logfile

--- app/start/global.php
+++ app/start/global.php
@@ -31,11 +31,11 @@ ClassLoader::addDirectories(array(
 |
 */
 
-//Log::useFiles(storage_path().'/logs/laravel.log');
+Log::useFiles(storage_path().'/logs/laravel.log');
 
-$logFile = 'laravel.log';
+/*logFile = 'laravel.log';
 
-Log::useDailyFiles(storage_path().'/logs/'.$logFile);
+Log::useDailyFiles(storage_path().'/logs/'.$logFile);*/
 
 /*
 |--------------------------------------------------------------------------
