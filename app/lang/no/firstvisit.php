470d883290e53313d6e22e1524afe185a0bcf543
Kim Syversen <kim.syversen@gmail.com>
2015-04-02 15:02:06 +0200

    Fix typos in translation

--- app/lang/no/firstvisit.php
+++ app/lang/no/firstvisit.php
@@ -2,7 +2,7 @@
 
 return array(
 	'firsttimestuff' => array(
-		'message' => 'Vil du ha en kort presentasjon av funksjonalitetet i denne applikasjonen? Hvis ikke så kan du alltis gå til bunnen av siden og finne igjen presentasjonen der.',
+		'message' => 'Vil du ha en kort presentasjon av funksjonaliteten i denne applikasjonen? Hvis ikke så kan du alltids gå til bunnen av siden og trykke på "Funksjonalitet i applikasjonen" ved et senere tidspunkt.',
 		'link' => 'Trykk her for en kort presentasjon.'
 	)
 );
