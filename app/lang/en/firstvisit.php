e7ea737e19d60bceaf83f5166d04c5dcb9f6dee9
Kim Syversen <kim.syversen@gmail.com>
2015-04-02 15:01:05 +0200

    Remove < from a translation

--- app/lang/en/firstvisit.php
+++ app/lang/en/firstvisit.php
@@ -3,6 +3,6 @@
 return array(
 	'firsttimestuff' => array(
 		'message' => 'Do you want a short presentation of the features of this application? If not, just scroll to the bottom of the page and click on "Features of this application" when you want.',
-		'link' => 'Give me a short presentation<'
+		'link' => 'Give me a short presentation'
 	)
 );
