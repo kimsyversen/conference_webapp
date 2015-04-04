8de197c8b50060991d19ae3204df374c9ec04cd8
Kim Syversen <kim.syversen@gmail.com>
2015-04-04 18:13:41 +0200

    Moved most? used items in navbar to right

--- app/views/conference/layouts/partials/nav.blade.php
+++ app/views/conference/layouts/partials/nav.blade.php
@@ -13,14 +13,15 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 @if(Session::has('conference_id'))
-                    <li><a href="{{ route('schedule_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferenceScheduleController@index') }}"><i class="glyphicon glyphicon-list-alt"></i>  {{ Lang::get('menu.schedule') }} </a> </li>
-                    <li><a href="{{ route('maps_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferenceMapsController@index') }}"><i class="glyphicon glyphicon-map-marker"></i>  {{ Lang::get('menu.maps') }}</a> </li>
+
                     <li><a href="{{ route('newsfeed_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferenceNewsFeedController@index') }}"><i class="glyphicon glyphicon-envelope"></i>  {{ Lang::get('menu.newsfeed') }}</a> </li>
 
                     @if(isset($authenticated) && $authenticated === true)
-                        <li><a href="{{ route('personal_schedule_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferencePersonalScheduleController@index') }}"><i class="glyphicon glyphicon-tasks"></i>  {{ Lang::get('menu.personal_schedule') }}</a> </li>
                         <li class="nav-item"><a href="{{ route('chats_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action(array('ConferenceChatsController@index', 'ConferenceChatsController@show')) }}"><i class="glyphicon glyphicon-comment"></i> {{ Lang::get('menu.messages') }}</a> </li>
+                        <li><a href="{{ route('personal_schedule_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferencePersonalScheduleController@index') }}"><i class="glyphicon glyphicon-tasks"></i>  {{ Lang::get('menu.personal_schedule') }}</a> </li>
                     @endif
+                    <li><a href="{{ route('maps_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferenceMapsController@index') }}"><i class="glyphicon glyphicon-map-marker"></i>  {{ Lang::get('menu.maps') }}</a> </li>
+                    <li><a href="{{ route('schedule_path', ['conference_id' => Session::get('conference_id')]) }}" class="{{ Active::action('ConferenceScheduleController@index') }}"><i class="glyphicon glyphicon-list-alt"></i>  {{ Lang::get('menu.schedule') }} </a> </li>
                 @endif
             </ul>
 
