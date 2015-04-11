c9f0cb84e59bcb052fe728768f6b5142e394d200
Kim Syversen <kim.syversen@gmail.com>
2015-04-11 19:38:42 +0200

    Users may zoom

--- app/views/conference/layouts/default.blade.php
+++ app/views/conference/layouts/default.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>ConferenceBook</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
     <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16">
     <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32">
