00b4f40b46a40b5d16f631b53cb214369f828fd5
Kim Syversen <kim.syversen@gmail.com>
2015-04-11 14:40:01 +0200

    Performance improvements

--- app/controllers/ConferenceScheduleController.php
+++ app/controllers/ConferenceScheduleController.php
@@ -18,6 +18,8 @@ class ConferenceScheduleController extends \BaseController {
 	{
 		$request = (new Uninett\Api\Request)->setMethod('GET');
 
+		$default_view = "";
+
 		if($this->userIsAuthenticated())
 			$request->setUrl("{$this->api_endpoint}/conferences/{$conference_id}/schedule/authenticated")
 				->setAccessTokenInHeader(AccessToken::get());
@@ -26,52 +28,54 @@ class ConferenceScheduleController extends \BaseController {
 
 		$response = $this->client->send($request);
 
-		$events = [];
-		//Incoming dates are in UTC timezone, mean they will represented in something like 2015-05-05UTC12:00:00
-		//Somehow, this does not show up on iphones and we need to format the date to this 2015-05-05T12:00:00
-		//We do not know if this affects other phones
-		foreach($response['data'] as $sessionGroup) {
-			foreach($sessionGroup['sessions'] as $session) {
-				$events[] = [
-					'title' => $session['title'],
-					'start' =>  (new  \Carbon\Carbon($session['start_date']['date']))->format(DateTime::ISO8601),
-					'end' =>   (new  \Carbon\Carbon($session['end_date']['date']))->format(DateTime::ISO8601),
-					'location' => $session['location'],
-					'url' => '/' . $session['links']['session']['uri'],
-					'color' => $this->decideColor($session['category'])
-				];
-			}
-		}
-
-
-
-		JavaScript::put([
-			'events' => $events,
-			'dayNames' => [
-				Lang::get('calendar.dayNames.monday'),
-				Lang::get('calendar.dayNames.tuesday'),
-				Lang::get('calendar.dayNames.wednesday'),
-				Lang::get('calendar.dayNames.thursday'),
-				Lang::get('calendar.dayNames.friday'),
-				Lang::get('calendar.dayNames.saturday'),
-				Lang::get('calendar.dayNames.sunday')
-			],
-			'buttonText' => [
-				'today' => Lang::get('calendar.buttonText.today'),
-				'week' => Lang::get('calendar.buttonText.week'),
-				'day' => Lang::get('calendar.buttonText.day'),
-				'month' => Lang::get('calendar.buttonText.month'),
-			]
-		]);
-
-		$default_view = "traditional";
-
 		if(Cookie::has('default_schedule_view'))
-			$default_view = Cookie::get('default_schedule_view');
+		{
+			$view = Cookie::get('default_schedule_view');
 
+			if($view != "traditional")
+			{
+				$events = [];
+				//Incoming dates are in UTC timezone, mean they will represented in something like 2015-05-05UTC12:00:00
+				//Somehow, this does not show up on iphones and we need to format the date to this 2015-05-05T12:00:00
+				//We do not know if this affects other phones
+				foreach($response['data'] as $sessionGroup) {
+					foreach($sessionGroup['sessions'] as $session) {
+						$events[] = [
+							'title' => $session['title'],
+							'start' =>  (new  \Carbon\Carbon($session['start_date']['date']))->format(DateTime::ISO8601),
+							'end' =>   (new  \Carbon\Carbon($session['end_date']['date']))->format(DateTime::ISO8601),
+							'location' => $session['location'],
+							'url' => '/' . $session['links']['session']['uri'],
+							'color' => $this->decideColor($session['category'])
+						];
+					}
+				}
+
+				JavaScript::put([
+					'events' => $events,
+					'dayNames' => [
+						Lang::get('calendar.dayNames.monday'),
+						Lang::get('calendar.dayNames.tuesday'),
+						Lang::get('calendar.dayNames.wednesday'),
+						Lang::get('calendar.dayNames.thursday'),
+						Lang::get('calendar.dayNames.friday'),
+						Lang::get('calendar.dayNames.saturday'),
+						Lang::get('calendar.dayNames.sunday')
+					],
+					'buttonText' => [
+						'today' => Lang::get('calendar.buttonText.today'),
+						'week' => Lang::get('calendar.buttonText.week'),
+						'day' => Lang::get('calendar.buttonText.day'),
+						'month' => Lang::get('calendar.buttonText.month'),
+					]
+				]);
+				$default_view = "calendar";
+
+			} else
+				$default_view = "traditional";
+		}
 		return View::make('conference.schedule.conference.index')->with(['data' => $response, 'default_view' => $default_view ]);
 	}
-
 
 	private function decideColor($category)
 	{
--- app/views/conference/events/eventGroup.blade.php
+++ app/views/conference/events/eventGroup.blade.php
@@ -10,87 +10,93 @@
 </div>
 
 {{ Form::open(['route' => 'ajax.user.change.view', 'id' => 'frm-view', 'class' => 'hidden']) }}
-<input id="view" name="view" type="hidden">
+    <input id="view" name="view" type="hidden">
 {{ Form::close()}}
 
 
-
-<div class="calendar-view {{ $default_view == "traditional" ? "toggled" : "toggle" }}" style="margin-top: 1em;">
-    <div class="col-xs-12">
-        <div id='calendar'></div>
-    </div>
-</div>
-
-
-
-<div class="traditional-view toggle {{ $default_view == "traditional" ? "toggle" : "toggled" }}">
-    <div class="row">
+@if($default_view == "calendar")
+    <div class="calendar-view {{ $default_view == "traditional" ? "toggled" : "toggle" }}" style="margin-top: 1em;">
         <div class="col-xs-12">
-            @include('conference.partials.options.filter')
+            @include('conference.events.partials.calendar')
         </div>
     </div>
+@endif
 
-    @include('conference.partials.options.filter-options')
-
-    @include('conference.partials.options.filter-options-count')
-
-    @foreach($data['data'] as $sessionGroup)
-        <div class="session-group" data-value="{{ ConferenceHelper::formatTimestamp($sessionGroup['start_date']['date'],'m/d')  }}">
-            @include('conference.partials.delimiter', ['text' => Lang::get('event.heading'), 'value' => ConferenceHelper::formatTimestamp($sessionGroup['start_date']['date'],'H:i')])
-
-            @foreach($sessionGroup['sessions'] as $session)
-                @include('conference.events.event', ['session' => $session, 'schedule_type' => $schedule_type])
-            @endforeach
+@if($default_view == "traditional")
+    <div class="traditional-view toggle {{ $default_view == "traditional" ? "toggle" : "toggled" }}">
+        <div class="row">
+            <div class="col-xs-12">
+                @include('conference.partials.options.filter')
+            </div>
         </div>
-    @endforeach
-</div>
+
+        @include('conference.partials.options.filter-options')
+
+        @include('conference.partials.options.filter-options-count')
+
+        @foreach($data['data'] as $sessionGroup)
+            <div class="session-group" data-value="{{ ConferenceHelper::formatTimestamp($sessionGroup['start_date']['date'],'m/d')  }}">
+                @include('conference.partials.delimiter', ['text' => Lang::get('event.heading'), 'value' => ConferenceHelper::formatTimestamp($sessionGroup['start_date']['date'],'H:i')])
+
+                @foreach($sessionGroup['sessions'] as $session)
+                    @include('conference.events.event', ['session' => $session, 'schedule_type' => $schedule_type])
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+@endif
+
+
+
+
+
 
 <script>
-    $(document).ready(function() {
-        addToHomescreen();
-        // page is now ready, initialize the calendar...
-            $('#buttonCalendar').click(function() {
-                $('.calendar-view').removeClass('toggled').addClass('toggle');
-                $('.traditional-view').addClass('toggled').removeClass('toggle');
-            });
+$(document).ready(function() {
+addToHomescreen();
+// page is now ready, initialize the calendar...
+   $('#buttonCalendar').click(function() {
+       $('.calendar-view').removeClass('toggled').addClass('toggle');
+       $('.traditional-view').addClass('toggled').removeClass('toggle');
+   });
 
-            $('#buttonAlerts').click(function() {
-                $('.calendar-view').removeClass('toggle').addClass('toggled');
-                $('.traditional-view').addClass('toggle').removeClass('toggled');
-            });
+   $('#buttonAlerts').click(function() {
+       $('.calendar-view').removeClass('toggle').addClass('toggled');
+       $('.traditional-view').addClass('toggle').removeClass('toggled');
+   });
 
 
 
-        $('#calendar').fullCalendar({
-            defaultView: 'agendaDay',
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'agendaWeek,agendaDay'
-            },
-            buttonText: {
-                today : Uninett.buttonText.today,
-                month : Uninett.buttonText.month,
-                week : Uninett.buttonText.week,
-                day: Uninett.buttonText.day
-            },
-            viewRender : function( view, element ) {
-                $.jStorage.set('defaultDate', $('#calendar').fullCalendar('getDate'));
-            },
-             defaultDate: $.jStorage.get('defaultDate', "") !== "" ? $.jStorage.get('defaultDate') : moment(),
-            dayNames : Uninett.dayNames,
-            height: 'auto',
-            allDayDefault: false,
-            allDaySlot: false,
-            firstDay: 1,
-            axisFormat: 'H:mm',
-            timeFormat: 'H(:mm)',
-            minTime: '06:00:00',
-            maxTime: '24:00:00',
-            slotDuration: '00:15:00',
-            events: Uninett.events
-        });
-    });
+$('#calendar').fullCalendar({
+   defaultView: 'agendaDay',
+   header: {
+       left: 'prev,next today',
+       center: 'title',
+       right: 'agendaWeek,agendaDay'
+   },
+   buttonText: {
+       today : Uninett.buttonText.today,
+       month : Uninett.buttonText.month,
+       week : Uninett.buttonText.week,
+       day: Uninett.buttonText.day
+   },
+   viewRender : function( view, element ) {
+       $.jStorage.set('defaultDate', $('#calendar').fullCalendar('getDate'));
+   },
+    defaultDate: $.jStorage.get('defaultDate', "") !== "" ? $.jStorage.get('defaultDate') : moment(),
+   dayNames : Uninett.dayNames,
+   height: 'auto',
+   allDayDefault: false,
+   allDaySlot: false,
+   firstDay: 1,
+   axisFormat: 'H:mm',
+   timeFormat: 'H(:mm)',
+   minTime: '06:00:00',
+   maxTime: '24:00:00',
+   slotDuration: '00:15:00',
+   events: Uninett.events
+});
+});
 
 </script>
 
