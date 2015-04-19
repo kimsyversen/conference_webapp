a180ddf36e67ef87dbd605d609e9821e789def1f
Kim Syversen <kim.syversen@gmail.com>
2015-04-19 17:12:29 +0200

    Bugfix

--- app/views/conference/partials/alerts/firstvisit.blade.php
+++ app/views/conference/partials/alerts/firstvisit.blade.php
@@ -1,5 +1,5 @@
 <div class="site-notice">
-    <div class="alert alert-info alert-dismissible animated fadeIn">
+{{--    <div class="alert alert-info alert-dismissible animated fadeIn">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <div class="bell animated flash">
             <i class="glyphicon glyphicon-bell"></i>
@@ -9,10 +9,10 @@
                 <a class="alert-link" href="{{ route('featurette_path')}}"> {{ Lang::get('firstvisit.firsttimestuff.link') }} </a>
             </p>
         </div>
-    </div>
+    </div>--}}
 
 
-{{--    <div class="alert alert-danger alert-dismissible animated fadeIn">
+    <div class="alert alert-danger alert-dismissible animated fadeIn">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <div class="bell animated flash">
             <i class="glyphicon glyphicon-bell"></i>
@@ -20,7 +20,7 @@
         <div class="message">
             <p> {{ Lang::get('firstvisit.firsttimestuff.guidelines.message') }} <a class="alert-link" href="{{ route('guideline_path') }}">{{ Lang::get('firstvisit.firsttimestuff.guidelines.link') }}</a> {{ Lang::get('firstvisit.firsttimestuff.guidelines.message2') }}  {{ Lang::get('firstvisit.firsttimestuff.guidelines.title') }} </p>
         </div>
-    </div>--}}
+    </div>
 
 
 </div>
