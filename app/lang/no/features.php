de17c5dc25b9a44d328403d292f51fa12fc680e0
Kim Syversen <kim.syversen@gmail.com>
2015-04-19 18:28:19 +0200

    Add calendar to features

--- app/lang/no/features.php
+++ app/lang/no/features.php
@@ -18,7 +18,7 @@ return array(
 			'1' => 'Du kan finne opptil fire typer hendelser i konferanseprogrammet.',
 			'2' => 'Alle hendelser har en lokasjon og ofte en beskrivelse som kan ses ved å klikke på ',
 			'3' => 'Hvis du er innlogget får du mulighet til å legge til hendelser i',
-			'4' => 'Hvis hendelsen er kansellert, vil du et rødt kryss og rød tekst. Du kan også dele på Facebook, Twitter og Google+. Om du ønsker å legge igjen en vurdering, klikk deg inn på en hvilken som helst hendelse. Der har du mulighet til å gi en 1-5 score samt legge ved en valgfri kommentar.',
+			'4' => 'Hvis hendelsen er kansellert, vil du et rødt kryss og rød tekst. Du kan også dele på Facebook, Twitter og Google+. Om du ønsker å legge igjen en vurdering, klikk deg inn på en hvilken som helst hendelse. Der har du mulighet til å gi en 1-5 score samt legge ved en valgfri kommentar. Du kan også bytte mellom kalender og den tradisjonell visning. Kalendervisning egner seg godt for å få en rask oversikt over hele konferanseprogrammet.',
 			'5' => array(
 				'1' => 'Du kan søke i hendelser med fritekst eller vise programmet basert på dag.',
 				'2' => 'Se etter',
