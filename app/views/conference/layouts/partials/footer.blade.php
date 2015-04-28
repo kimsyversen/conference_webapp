d1bf7cf63420eff1ca925e1372b5a7ef1ecb513f
Kim Syversen <kim.syversen@gmail.com>
2015-04-28 11:12:07 +0200

    Remove home from footer

--- app/views/conference/layouts/partials/footer.blade.php
+++ app/views/conference/layouts/partials/footer.blade.php
@@ -6,7 +6,6 @@
                 <li class="nav-item"><i class="glyphicon glyphicon glyphicon-warning-sign"></i> <a href="{{ route('guideline_path') }}">{{ Lang::get('menu.guidelines') }}</a> </li>
                 <li class="nav-item"><i class="glyphicon glyphicon-info-sign"></i> <a href="{{ route('about_path') }}"> {{ Lang::get('menu.about_application') }}</a></li>
                 <li class="nav-item"><i class="glyphicon glyphicon glyphicon-question-sign"></i> <a href="{{ route('featurette_path') }}">{{ Lang::get('menu.features_application') }}</a></li>
-                <li class="nav-item"><i class="glyphicon glyphicon glyphicon-home"></i> <a href="{{ route('conferences_path') }}">{{ Lang::get('menu.home') }}</a> </li>
             </ul>
         </div>
     </div>
