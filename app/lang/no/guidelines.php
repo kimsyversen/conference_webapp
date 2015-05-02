11b49668884869bc3f81d5571cbad39b2b0e2f0e
Kim Syversen <kim.syversen@gmail.com>
2015-05-02 08:42:27 +0200

    Typofix

--- app/lang/no/guidelines.php
+++ app/lang/no/guidelines.php
@@ -4,7 +4,7 @@ return array(
 	'p' => 'Applikasjonen må lagre noe data om deg for at den skal kunne benyttes.',
 	'list' => array(
 		'1' => 'Cookies benyttes for personlige innstillinger i applikasjonen (språkpreferanser). Disse lagres på din enhet og leses av konferanseappen.',
-		'2' => 'Ved opprettelse av brukerkonto vil data epost og passord deg bli lagret i databasen. Alle passord er kryptert. Du vil ikke motta spam, og epost-adresser brukes kun for at du skal kunne logge inn og bruke applikasjonen.',
+		'2' => 'Ved opprettelse av brukerkonto vil data epost og passord om deg bli lagret i databasen. Alle passord er kryptert. Du vil ikke motta spam, og epost-adresser brukes kun for at du skal kunne logge inn og bruke applikasjonen.',
 		'3' => 'Funksjonalitet som krever innlogging, og hvor data lagres er: Min agenda, brukerkonto og vurderinger. Ved vurderinger vil vurderingen også bli lagret, og denne kan spores tilbake til epost. Dette vil imidlertid ikke bli gjort siden vi bare er interessert i vurderingene.',
 		'4' => 'IP-adresser loggføres, men de benyttes ikke til noe formål.',
 
