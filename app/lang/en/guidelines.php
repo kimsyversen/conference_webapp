4c74e7c021536d6517715641100d05cb73d465e3
Kim Syversen <kim.syversen@gmail.com>
2015-05-02 14:27:23 +0200

    Typos

--- app/lang/en/guidelines.php
+++ app/lang/en/guidelines.php
@@ -3,7 +3,7 @@ return array(
 	'title' => 'Application guidelines',
 	'p' => 'This application stores some data about you to be usable.',
 	'list' => array(
-		'1' => 'Cookies are used to store personal settings on each device you use to visit this page. As of today we use it to store language settings.',
+		'1' => 'Cookies are used to store personal settings on each device you use to visit this page. As of today we use it to store language settings, determine if you previously have used the application, and the schedule layout.',
 		'2' => 'If you choose you create an account, we need to store your email and password. Your password is always encrypted.  You will never receive spam and email-adresses are only used for the purpose to unique identify you to provide you the application features.',
 		'3' => 'Functionality which requires login, and where data is stored are: Your personal agenda, user account variables and all ratings. Ratings can be traced back to your email, but it will never be done.',
 		'4' => 'All IP-addresses are logged by the web server (not directly this application), but they are not used to any purpose.',
